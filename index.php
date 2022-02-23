f646ffbc47d6b2170fa2a7e8b8e5c91b51daa118
nhonnam <nobeltia@gmail.com>
2022-02-23 08:42:15 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+ <head>
+  <title>1st PHP Page</title>
+ </head>
+ <body>
+    <?php 
+    echo'<h1>Welcome to ATN!</h1>';
+    // define("NAM","Cau Be Vuoi Ve");
+    // echo NAM;
+    // echo"<h1>$text</h1>";
+
+?>
+<button><a href="login.php">Login</button>
+</form>
+ </body>
+</html>
